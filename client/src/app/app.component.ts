01b61c14461d8f9bc50297c963c0c11289cd45f0
DESKTOP-16P13L7\Home <saidi.aladdin4@gmail.com>
2021-08-12 11:43:01 +0100

    end of section 14

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
  
 import { Component, OnInit } from '@angular/core';
+import { BasketService } from './basket/basket.service';
  
 
 @Component({
@@ -13,10 +14,18 @@ export class AppComponent implements OnInit {
   title = 'ChedDarek';
  
 
-  constructor( ) {}
+  constructor( private basketService:BasketService) {}
 
   ngOnInit(): void {
- 
- 
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) {
+      this.basketService.getBasket(basketId).subscribe(()=> {
+        console.log('initialised basket');
+        
+      },error => {
+        console.log(error);
+        
+      })
+    }
   }
 }
